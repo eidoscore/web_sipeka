ba78650b512b3c7ce2c820391b24e33eb21fa122
eidoscore <buatpengaman@yahoo.com>
2020-05-27 19:42:02 +0700

    Menambahkan Menu management Dashboard Admin/User berdasarkan role_id Database

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -12,35 +12,51 @@
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Admin
-    </div>
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
+    <!-- Query Menu -->
+    <?php
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+    $role_id = $this->session->userdata('role_id');
+    $qeuryMenu = "SELECT `user_menu`.`id`, `menu`
+                    FROM `user_menu` JOIN `user_access_menu` 
+                        ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                    WHERE `user_access_menu`.`role_id` = $role_id
+                ORDER BY `user_access_menu`.`menu_id` ASC
+    ";
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        User
-    </div>
+    $menu = $this->db->query($qeuryMenu)->result_array();
+    ?>
+    <!-- Looping Menu -->
+    <?php foreach ($menu as $m) : ?>
+        <div class="sidebar-heading">
+            <?= $m['menu']; ?>
+        </div>
 
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-user"></i>
-            <span>User Profile</span></a>
-    </li>
+        <!-- Siapkan sub-menu sesuai sub menu -->
+        <?php
+        $menuId = $m['id'];
+        $querySubMenu = "SELECT * 
+                            FROM `user_sub_menu`
+                            WHERE `menu_id` = $menuId
+                            AND   `is_active` = 1
+                        ";
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+        $subMenu = $this->db->query($querySubMenu)->result_array();
+        ?>
+
+        <?php foreach ($subMenu as $sm) : ?>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+                    <i class="<?= $sm['icon']; ?>"></i>
+                    <span><?= $sm['title']; ?></span></a>
+            </li>
+        <?php endforeach; ?>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+    <?php endforeach; ?>
 
     <!-- Nav Item - Charts -->
     <li class="nav-item">
