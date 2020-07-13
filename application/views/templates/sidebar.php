22a72747545d87c79fb1c54022946a8015dc3548
eidoscore <buatpengaman@yahoo.com>
2020-07-13 18:56:45 +0700

    Menambahkan submenu Management

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -46,32 +46,36 @@
 
         <?php foreach ($subMenu as $sm) : ?>
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
+            <?php if ($title == $sm['title']) : ?>
+                <li class="nav-item active">
+                <?php else : ?>
+                <li class="nav-item">
+                <?php endif; ?>
                 <a class="nav-link" href="<?= base_url($sm['url']); ?>">
                     <i class="<?= $sm['icon']; ?>"></i>
                     <span><?= $sm['title']; ?></span></a>
-            </li>
+                </li>
+            <?php endforeach; ?>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
         <?php endforeach; ?>
 
+        <!-- Nav Item - Charts -->
+        <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('auth/login') ?>">
+                <i class="fas fa-fw fa-sign-out-alt"></i>
+                <span>Log out</span></a>
+        </li>
+
         <!-- Divider -->
-        <hr class="sidebar-divider">
+        <hr class="sidebar-divider d-none d-md-block">
 
-    <?php endforeach; ?>
-
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('auth/login') ?>">
-            <i class="fas fa-fw fa-sign-out-alt"></i>
-            <span>Log out</span></a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
-
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
 
 </ul>
 <!-- End of Sidebar -->
